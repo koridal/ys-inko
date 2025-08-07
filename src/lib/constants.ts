561f5b501dc4b9d4d2213939c37b13262b4547ed
koridal <hjpark002@gmail.com>
2025-08-07 12:06:56 +0900

    DarkMode & Change font

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+  { title: 'Home', href: '/' },
+  { title: 'About', href: '/about' },
+  { title: 'Services', href: '/services' },
+  { title: 'Projects', href: '/projects' },
+  { title: 'Contact', href: '/contact' },
+]
